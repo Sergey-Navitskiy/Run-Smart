d19acd48396306c262fa86350cb4e8f09d6d431e
Sergey-Navitskiy <126409337+Sergey-Navitskiy@users.noreply.github.com>
2023-12-13 20:20:29 +0300

    tabs button scripts

--- js/script.js
+++ js/script.js
@@ -1,24 +1,50 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron left solid.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron right solid.png"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                  dots: true,
-                  arrows: false
-                }
-              },
-          
-        ]
-      });
-
-    $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
-      $(this)
-        .addClass('active').siblings().removeClass('active')
-        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-      });
+$(document).ready(function () {
+  $(".carousel__inner").slick({
+    speed: 1200,
+    adaptiveHeight: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="icons/chevron left solid.png"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="icons/chevron right solid.png"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          arrows: false,
+        },
+      },
+    ],
   });
+
+  $("ul.catalog__tabs").on(
+    "click",
+    "li:not(.catalog__tab_active)",
+    function () {
+      $(this)
+        .addClass("catalog__tab_active")
+        .siblings()
+        .removeClass("catalog__tab_active")
+        .closest("div.container")
+        .find("div.catalog__content")
+        .removeClass("catalog__content_active")
+        .eq($(this).index())
+        .addClass("catalog__content_active");
+    }
+  );
+
+
+  function toogleSlide(item) {
+    $(item).each(function(i){
+      $(this).on('click', function(e){
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
+      })
+    })
+  }
+
+  toogleSlide('.catalog-item__link');
+  toogleSlide('.catalog-item__back');
+});
+
